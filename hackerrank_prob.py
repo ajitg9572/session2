cc84ed4a9e240837731ec22612b73cc55bf92d2e
ajit kumar <ajitg9572@gmail.com>
2020-01-23 23:15:38 +0530

    firstcommit

---
+++ hackerrank_prob.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+# declare and initialize a list
+fruits = ["apple","mango","guava","grapes","pinapple"]
+
+# pritning type of fruits
+print (type(fruits))
+
+# printing value
+for fruit in fruits:
+    print(fruit)
